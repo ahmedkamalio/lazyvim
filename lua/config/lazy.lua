e63b2f82ceed93731250892df3e3b80d1d566001
Ahmed Kamal <info@ahmedmkamal.com>
2024-05-30 01:58:19 +0300

    go-templ support

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -1,10 +1,16 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  -- bootstrap lazy.nvim
-  -- stylua: ignore
-  vim.fn.system({ "git", "clone", "--filter=blob:none", "https://github.com/folke/lazy.nvim.git", "--branch=stable", lazypath })
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable",
+    lazypath,
+  })
 end
+
 vim.opt.rtp:prepend(vim.env.LAZY or lazypath)
 
 require("lazy").setup({
@@ -50,9 +56,9 @@ require("lazy").setup({
   },
 })
 
--- Associate .gohtml files with HTML for TreeSitter.
 vim.filetype.add({
   extension = {
     gohtml = "html",
+    templ = "templ",
   },
 })
