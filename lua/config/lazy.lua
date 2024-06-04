82127e76a2c77640cd11efa7f0c3980f543435be
Ahmed Kamal <info@ahmedmkamal.com>
2024-06-04 19:27:17 +0300

    Update plugins

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -16,7 +16,7 @@ vim.opt.rtp:prepend(vim.env.LAZY or lazypath)
 require("lazy").setup({
   spec = {
     -- add LazyVim and import its plugins
-    { "LazyVim/LazyVim", import = "lazyvim.plugins", commit = "da7b7738eb84b00c949f48afabebd39266e61d75" },
+    { "LazyVim/LazyVim", import = "lazyvim.plugins" },
     -- import any extras modules here
     { import = "lazyvim.plugins.extras.lang.docker" },
     { import = "lazyvim.plugins.extras.lang.go" },
